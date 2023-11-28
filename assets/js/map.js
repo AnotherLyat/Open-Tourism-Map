34c839e46b28c44180ecaf2938be6bfc072e85d5
Leonardo Yuji Alves Tanaka <leonardo.tanaka@convergint.com>
2023-11-28 13:29:51 -0300

    capability to delete and a switch

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
 
+  // Variable to store the current marker
+  var currentMarker;
+
   // Add double click event listener to the map
   mymap.on('dblclick', function (event) {
     // Get the clicked coordinates
@@ -14,32 +17,49 @@ document.addEventListener('DOMContentLoaded', function () {
     var lng = event.latlng.lng;
 
     // Create a marker and add it to the map
-    var marker = L.marker([lat, lng]).addTo(mymap);
+    currentMarker = L.marker([lat, lng]).addTo(mymap);
 
     // Create a custom popup with HTML content
-    marker.bindPopup(`
+    currentMarker.bindPopup(`
       <div style="max-width: 200px;">
         <button style="float: right;" onclick="saveMarkerInfo()">Save</button>
+        <button id="deleteButton" onclick="deleteMarker()">Delete</button>
         <div>
           <p>Marker Information:</p>
           <input type="text" id="markerTextbox" placeholder="Enter information">
         </div>
         <div style="text-align: left; margin-top: 10px;">
-          <label for="switchButton">Switch:</label>
-          <label class="switch">
-            <input type="checkbox" id="switchButton">
-            <span class="slider"></span>
-          </label>
+          <button id="stateButton" onclick="toggleState()">
+            <span id="stateIcon" class="play-icon">▶️</span>
+          </button>
         </div>
       </div>
     `).openPopup();
   });
+
+  window.toggleState = function () {
+    var stateButton = document.getElementById('stateButton');
+    var stateIcon = document.getElementById('stateIcon');
+
+    if (stateButton.classList.contains('playing')) {
+      stateButton.classList.remove('playing');
+      stateIcon.textContent = '▶️'; // Play icon
+      stateButton.style.backgroundColor = '#3498db'; // Default color
+    } else {
+      stateButton.classList.add('playing');
+      stateIcon.textContent = '⏸️'; // Pause icon
+      stateButton.style.backgroundColor = '#e74c3c'; // Active color
+    }
+  };
+
+  window.saveMarkerInfo = function () {
+    var markerTextboxValue = document.getElementById('markerTextbox').value;
+    alert('Marker Information: ' + markerTextboxValue);
+  };
+
+  window.deleteMarker = function () {
+    if (currentMarker) {
+      mymap.removeLayer(currentMarker);
+    }
+  };
 });
-
-window.saveMarkerInfo = function () {
-  var markerTextboxValue = document.getElementById('markerTextbox').value;
-  var switchState = document.getElementById('switchButton').checked;
-
-  alert('Marker Information: ' + markerTextboxValue + '\nSwitch State: ' + switchState);
-
-};
