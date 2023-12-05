bbf37cc1efef1bb704fc0645cedec13cf2248793
Fábio Veiga <veigabfabio@hotmail.com>
2023-12-05 12:42:53 -0300

    comentários

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,43 +1,86 @@
+
+// Indice
+//  #1 - inicializando overlay
+//  #2 - Mobile
+//  #3 - Desktop
+
+
+
+
+
+/* * * * * * * *
+ *  #1
+ *  Inicializando o overlay
+ *      - Inicializa variaveis para animação e reposiciona elementos
+ *        necessários
+ *
+ */
 // Reposiciona o 'zoom' para a direita, para que o overlay não fique na frente.
 document.querySelector("body").onload = function () {
     document.querySelector(".leaflet-left").classList.add('leaflet-right')
     document.querySelector(".leaflet-left").classList.remove('leaflet-left')
 }
 
-// Elemento do overlay
+// Elemento da janela overlay
 var overlay = document.querySelector(".map-overlay");
 
 // Variaveis para controlar a animação.
 var arrowPosition = 0;
 var isTransitioning = false;
 
+
+
+
+
+/* * * * * * * *
+ *  #2
+ *  Algoritmos da versão mobile
+ *
+ *
+ */
+
 // Elementos da versão mobile
 var overlayButtonMobile = document.querySelector(".overlay-drag-mobile");
 var arrowMobile = overlayButtonMobile.children[0];
 // Evento 'click' (Mobile)
 overlayButtonMobile.addEventListener("click", function (event) {
+    // Só executa se isTransitioning = false
     if (isTransitioning == false) {
+        // Assim que executa, não permite outra execução simultânea
         isTransitioning = true;
+        // Inicializa posição baseado na classe
         var direction = arrowMobile.classList.contains("fa-arrow-up") ? 'up' : 'down';
         var startPoint = direction == 'up' ? -240 : 0;
 
+        // Invervalo: resumidamente um loop com delay entre as execuções
         var intervalo = setInterval(() => {
+            // Anima ↑
             if (direction == 'up') {
-                // Anima ↑
+                // troca sentido da seta
                 trocaSeta()
+                // incrementa posição da janela a cada execução...
                 startPoint += 2;
+                // e aplica o novo estilo
                 overlay.style.marginBottom = startPoint + 'px';
+                // Nesse caso, quando o valor for 0...
                 if (startPoint == 0) {
+                    // Para o intervalo e permite outra execução
                     isTransitioning = false;
                     clearInterval(intervalo);
                 }
-            } else {
-                // Anima ↓
+            }
+            // Anima ↓
+            else if (direction == 'down') {
+                // Decrementa posicao da janela a cada execução
                 startPoint -= 2;
+                // aplica novo estilo
                 overlay.style.marginBottom = startPoint + 'px';
+                // Nesse caso, quando o valor for -240
                 if (startPoint == -240) {
+                    // Para o intervalo e permite outra execução
                     isTransitioning = false;
                     clearInterval(intervalo);
+                    // Nesse caso, só troca o sentido da seta no final do loop
                     trocaSeta();
                 }
             }
@@ -45,6 +88,7 @@ overlayButtonMobile.addEventListener("click", function (event) {
 
         // Troca o sentido da seta (↑, ↓)
         function trocaSeta() {
+            // Resumidamente, usa o sentido atual para selecionar o sentido oposto.
             if (direction == 'up') {
                 arrowMobile.classList.remove("fa-arrow-up");
                 arrowMobile.classList.add("fa-arrow-down");
@@ -55,6 +99,17 @@ overlayButtonMobile.addEventListener("click", function (event) {
         }
     }
 })
+
+
+
+
+
+/* * * * * * * *
+ *  #3
+ *  Algoritmos da versão desktop
+ *
+ *
+ */
 
 // Elementos da versão desktop
 var overlayButtonDesktop = document.querySelector(".overlay-drag-desktop");
@@ -103,14 +158,14 @@ overlayButtonDesktop.addEventListener('click', function () {
 })
 
 window.addEventListener("resize", () => {
-    if (window.innerWidth <= 650){
-        if (arrowDesktop.classList.contains("fa-arrow-left")){
+    if (window.innerWidth <= 650) {
+        if (arrowDesktop.classList.contains("fa-arrow-left")) {
             overlay.style.marginLeft = '0px'
-        }else if (overlay.style.marginLeft == '-400px'){
+        } else if (overlay.style.marginLeft == '-400px') {
             overlay.style.marginLeft = '0px'
         }
-    }else{
-        if (arrowDesktop.classList.contains("fa-arrow-right")){
+    } else {
+        if (arrowDesktop.classList.contains("fa-arrow-right")) {
             overlay.style.marginLeft = '-400px'
         }
     }
