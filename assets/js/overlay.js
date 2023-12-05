fc2fbf250cf2fda426143345b0d52ff2aedccc27
Fábio Veiga <veigabfabio@hotmail.com>
2023-12-05 12:47:08 -0300

    comentários

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -116,36 +116,50 @@ var overlayButtonDesktop = document.querySelector(".overlay-drag-desktop");
 var arrowDesktop = overlayButtonDesktop.children[0];
 // Evento 'click' (desktop)
 overlayButtonDesktop.addEventListener('click', function () {
+    // Só executa se isTransitioning = false
     if (isTransitioning == false) {
+        // Assim que executa, não permite outra execução simultânea
         isTransitioning = true;
+        // Inicializa posição baseado na classe
         var direction = arrowDesktop.classList.contains("fa-arrow-right") ? 'right' : 'left';
         var startPoint = direction == 'right' ? -400 : 0;
-        console.log(direction);
 
+        // Invervalo: resumidamente um loop com delay entre as execuções
         var intervalo = setInterval(() => {
+            // Anima →
             if (direction == 'right') {
-                // Anima →
+                // troca sentido da seta
                 trocaSeta();
+                // incrementa posição da janela a cada execução...
                 startPoint += 4;
+                // aplica novo estilo
                 overlay.style.marginLeft = startPoint + 'px';
+                // Nesse caso, quando o valor for 0...
                 if (startPoint == 0) {
+                    // para o intervalo e permite outra execução
                     clearInterval(intervalo);
                     isTransitioning = false;
                 }
-            } else {
-                // Anima ←
+            }
+            // Anima ←
+            else if (direction == 'left') {
+                // Decrementa posição da janela a cada execução
                 startPoint -= 4;
-                console.log("here")
+                // aplica novo estilo
                 overlay.style.marginLeft = startPoint + 'px';
+                // nesse caso, quando o valor for -400...
                 if (startPoint == -400) {
+                    // para o intervalo e permite outra execução
                     clearInterval(intervalo);
                     isTransitioning = false;
+                    // nesse caso, só troca o sentido da seta no final do intervalo
                     trocaSeta();
                 }
             }
         }, 1);
         // Troca o sentido da seta (→, ←)
         function trocaSeta() {
+            // Resumidamente, usa o sentido atual para selecionar o sentido oposto.
             if (direction == 'left') {
                 arrowDesktop.classList.remove("fa-arrow-left");
                 arrowDesktop.classList.add("fa-arrow-right");
@@ -157,6 +171,8 @@ overlayButtonDesktop.addEventListener('click', function () {
     }
 })
 
+//  Essa função verifica constantemente alterações na largura da tela para posicionar corretamente
+// os elementos dependendo da versão (desktop/mobile)
 window.addEventListener("resize", () => {
     if (window.innerWidth <= 650) {
         if (arrowDesktop.classList.contains("fa-arrow-left")) {
